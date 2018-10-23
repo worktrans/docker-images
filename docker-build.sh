50c27675c85e8b528842e717a2e61c21152a1b45
huchenlong <hcl0810@163.com>
2018-10-23 11:16:03 +0800

    fixed bug

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,6 @@
 
 docker build -t worktrans/ubuntu:18.04 ubuntu/bionic/ \
 && docker build -t worktrans/java:8-jre java/8-jre/ \
-&& docker build -t worktrans/tomcat:8.5-jre-8 tomcat/8.5/jre-8/ \
+&& docker build -t worktrans/tomcat:8.5-jre-8 tomcat/8.5/jre-8/
 
-&& docker images | grep 'worktrans'
+docker images | grep 'worktrans'
