3bcb101a84c93f35a551244684e415823cecb390
huchenlong <hcl0810@163.com>
2018-10-23 10:32:50 +0800

    add docker build bash

---
+++ docker-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker build -t worktrans/ubuntu:18.04 ubuntu/bionic/
+docker build -t worktrans/java:8-jre java/8-jre/
+docker build -t worktrans/tomcat:8.5-jre-8 tomcat/8.5/jre-8/
+
+docker images | grep 'worktrans'
